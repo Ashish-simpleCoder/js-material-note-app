381449089a9752728fca77cf7c542817f3b669fc
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2021-08-22 03:24:07 +0530

    Updates

--- src/handleSearch.js
+++ src/handleSearch.js
@@ -1,9 +1,20 @@
-export default function searchNote(val,note_containers){
+// export default function searchNote(val,note_containers){
+//     for(let i=0; i<note_containers.length; i++){
+//         const str2 = note_containers[i].children[0].innerText
+//         const str = note_containers[i].children[1].innerText
+//         if(str2.includes(val) || str.includes(val)){
+//             return i
+//         }
+//     }
+// }
+export default function searchNote(val,note_containers,arr,j){
     for(let i=0; i<note_containers.length; i++){
-        const str2 = note_containers[i].children[0].innerText
-        const str = note_containers[i].children[1].innerText
-        if(str2.includes(val) || str.includes(val)){
-            return i
+        let str = note_containers[i].children[0].innerText
+        let str2 = note_containers[i].children[1].innerText
+        if(str.includes(val) || str2.includes(val)){
+            arr[j] = i
+            j++
         }
     }
+    return arr
 }
--- src/index.js
+++ src/index.js
@@ -10,24 +10,37 @@ addEventListener('load',async()=>{
     loadNoteFromLocalStorage.default()
 })
 
-
 search_keyword.addEventListener('input',async (e)=>{
-
     const note_containers = document.querySelectorAll('.each_note_container')
+    let arr =[]
+    let j=0
     const handleSearch = await import('./handleSearch.js')
-    const matched = handleSearch.default(e.target.value,note_containers)
-
+    const matched = handleSearch.default(e.target.value,note_containers,arr,j)
+    console.log(matched)
     if(e.target.value == ''){
         note_containers.forEach(note=>{
             note.style.display='block'
+            note.style.border='none'
         })
     }else{
-        for(let i=0; i<note_containers.length;i++){
-            if(i !== matched){
-                note_containers[i].style.display = 'none'
-            }
-            if(i == matched){
-                note_containers[i].style.display = 'block'
+        // for(let i=0; i<note_containers.length;i++){
+        //     if(i !== matched){
+        //         note_containers[i].style.display = 'none'
+        //     }
+        //     if(i == matched){
+        //         note_containers[i].style.display = 'block'
+        //     }
+        // }
+        note_containers.forEach(note=>note.style.display='none')
+
+        for(let i=0; i<matched.length;i++){
+            for(let k=0;k<note_containers.length;k++){
+                if(matched[i] == k){
+                    // log(matched[i],k)
+                    note_containers[k].style.border = '1px solid red'
+                    note_containers[k].style.display = 'block'
+                }
+
             }
         }
     }
