fdead8a1a1a3e18c5507ca0a9ef888c220436883
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2021-08-25 07:13:51 +0530

    Updates

--- src/noteDelete.js
+++ src/noteDelete.js
@@ -1,15 +1,16 @@
 export default function noteDeleter(div){
-    setTimeout(()=>document.body.classList.remove('body_relative'),100)
 
+    console.log('deleting')
     let notes = JSON.parse(localStorage.getItem('notes'))
 
     notes = notes.filter(note=>note.id != div.id)
     localStorage.setItem('notes',JSON.stringify(notes))
     div.remove()
+    setTimeout(()=>document.body.classList.remove('body_relative'),100)
 
 
-    if(notes.length === 0){
-        dummy.style.display = 'block'
-        search_form.style.opacity = '0'
-    }
+    // if(notes.length === 0){
+    //     dummy.style.display = 'block'
+    //     search_form.style.opacity = '0'
+    // }
 }
