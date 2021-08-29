550eb837ab0cc9a5b6113ca06e6a058f5266674f
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2021-08-29 10:37:49 +0530

    Updates

--- src/noteRenderer.js
+++ src/noteRenderer.js
@@ -19,26 +19,24 @@ export default async function noteRenderer(note_obj,fromLocal){
                         <button id='delete_btn'><span><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" class="svg-inline--fa fa-trash fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path></svg></span></button>
                      </div>`
     div.appendChild(span)
+
     if(fromLocal==true){
         note_output_section.append(div)
         let divs = []
         divs.push(div)
         if(divs){
             const loazyLoder = await import('./lazyLoader.js')
-            divs.forEach((div)=>loazyLoder.default(div))
+            divs.forEach(loazyLoder.default)
         }
-    }
-    else {
+    }else {
         div.appendChild(span)
         note_output_section.insertAdjacentElement('afterbegin',div)
     }
     dummy.style.display = 'none'
-
     search_form_title.style.display = 'block'
     search_keyword.style.display = 'block'
 
     note_title.value='', note_content.value='' //emptying the inputs
-    const save_btn = div.querySelector('#save_btn')
 
     div.querySelector('div h3').addEventListener('click',async function(e){
         e.stopPropagation()
@@ -62,14 +60,13 @@ export default async function noteRenderer(note_obj,fromLocal){
         saveEditedNote.default(div)
     })
 
-    save_btn.addEventListener('click',async(e)=>{
+    div.querySelector('#save_btn').addEventListener('click',async(e)=>{
         // e.stopPropagation()
-        // if(div.children[0].getAttribute('contenteditable') === 'false') return
         const saveEditedNote = await import('./saveEditedNote.js')
         saveEditedNote.default(div)
     })
 
-    // const delete_btn = div.querySelector('#delete_btn')
+
     div.querySelector('#delete_btn').addEventListener('click',async (e)=>{
         e.stopPropagation()
         const deleteNote = await import('./noteDelete.js')
