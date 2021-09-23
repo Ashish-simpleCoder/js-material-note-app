5e699b3618b86a2f92e25d94bc4f6e5970962487
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2021-09-23 13:40:32 +0530

    deletion animation

--- dist/src/handleSearch.js
+++ dist/src/handleSearch.js
@@ -5,9 +5,15 @@ export default function searchNote(val,note_containers,arr,j){
             val = val.replace("  "," ")
         }
     }
+
     for(let i=0; i<note_containers.length; i++){
         let str = note_containers[i].children[0].innerText.toLowerCase().trim()
         let str2 = note_containers[i].children[1].innerText.toLowerCase().trim()
+        if(str2.includes("\n")){
+            for(let i=0;i<str2.length; i++){
+                str = str2.replace("\n"," ")
+            }
+        }
         if(str.includes(val) || str2.includes(val)){
             arr[j] = i
             j++
