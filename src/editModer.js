44a3a8ed865b2f1e752238623708b1b25e4cf8cc
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-01-07 20:39:53 +0530

    Updates

--- src/editModer.js
+++ src/editModer.js
@@ -1,13 +1,10 @@
 export default function editModer(id,e){
-    // console.log(div)
-    // return
     const div = document.getElementById(id)
-    console.log(div)
     div.style.opacity = 0
     const {top, left, width, height} = div.getBoundingClientRect()
 
     const editor_mode = document.querySelector('.editor_mode')
-    editor_mode.id = div.id
+    editor_mode.id = id
     editor_mode.style.top = top + 'px'
     editor_mode.style.left = left + 'px'
     editor_mode.style.width = width +'px'
@@ -17,26 +14,15 @@ export default function editModer(id,e){
 
     setTimeout(()=>{
         document.body.classList.add('edit_mode')
-        let x = e.clientX - left
-        let y = e.clientY - top
-        const span = document.createElement('span')
-        span.style.left = x+'px'
-        span.style.top = y+'px'
+        const obj = {e,editor_mode,left,top}
 
-        editor_mode.appendChild(span)
-        setTimeout(()=>span.remove(),500)
+        // generateAndAppendSpan(obj)
 
         editor_mode.children[0].textContent = div.children[0].textContent
         editor_mode.children[1].value = div.children[1].textContent
-        editor_mode.children[0].contentEditable = true
-        editor_mode.children[1].contentEditable = true
     },20)
 
-    addEventListener('keyup',async(e)=>{
-        if(e.key === 'Escape'){
-           saveNotes()
-        }
-    })
+    addEventListener('keyup',(e)=>e.key === 'Escape' && saveNotes())
     editor_mode.querySelector('.save_btn').addEventListener('click',saveNotes)
     modal_wrapper.addEventListener('click',saveNotes)
 
@@ -49,15 +35,29 @@ export default function editModer(id,e){
         let updated_note = JSON.parse(localStorage.getItem('notes'))
         updated_note = updated_note.filter(({id:c})=>c===id)
 
+        // updating the editing notes details
         div.children[0].textContent = updated_note[0].title
         div.children[1].textContent = updated_note[0].content
         document.body.classList.remove('edit_mode')
 
+        // restting the modal
         setTimeout(()=>{
             editor_mode.style.display = 'none'
             div.style.opacity = 1
             editor_mode.children[0].textContent = ''
             editor_mode.children[1].value = ''
-        },401)
+        },501)
     }
+}
+
+
+function generateAndAppendSpan({e, editor_mode, left, top}){
+    let x = e.clientX - left
+    let y = e.clientY - top
+    const span = document.createElement('span')
+    span.style.left = x+'px'
+    span.style.top = y+'px'
+
+    editor_mode.appendChild(span)
+    setTimeout(()=>span.remove(),500)
 }
