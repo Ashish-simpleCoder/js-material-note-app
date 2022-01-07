d377b901126787d21eaebb5feedb9751a5094afd
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-01-07 12:19:33 +0530

    Updates

--- src/editModer.js
+++ src/editModer.js
@@ -1,8 +1,10 @@
 export default function editModer(div,c1,c2,e,save_btn){
+    div.style.opacity = 0
+    const {top, left, width, height} = div.getBoundingClientRect()
+
     const editor_mode = document.querySelector('.editor_mode')
     editor_mode.id = div.id
 
-    const {top, left, width, height} = div.getBoundingClientRect()
     editor_mode.style.top = top + 'px'
     editor_mode.style.left = left + 'px'
     editor_mode.style.width = width +'px'
@@ -10,33 +12,32 @@ export default function editModer(div,c1,c2,e,save_btn){
     editor_mode.style.display = 'block'
 
 
-    div.style.opacity = 0
     setTimeout(()=>{
         document.body.classList.add('edit_mode')
-        editor_mode.children[0].innerHTML = div.children[0].textContent
-        editor_mode.children[1].innerHTML = div.children[1].textContent
-        editor_mode.children[0].contentEditable = true
-        editor_mode.children[1].contentEditable = true
-
         let x = e.clientX - left
         let y = e.clientY - top
         const span = document.createElement('span')
-        span.classList.add('ripple_effect')
+        // span.classList.add('ripple_effect')
         span.style.left = x+'px'
         span.style.top = y+'px'
 
         editor_mode.appendChild(span)
         setTimeout(()=>span.remove(),500)
 
+
+        editor_mode.children[0].innerHTML = div.children[0].textContent
+        editor_mode.children[1].innerHTML = div.children[1].textContent
+        editor_mode.children[0].contentEditable = true
+        editor_mode.children[1].contentEditable = true
     },20)
+
     addEventListener('keyup',async(e)=>{
         if(e.key === 'Escape'){
            saveNotes()
         }
     })
-    editor_mode.querySelector('.save_btn').addEventListener('click',async()=>{
-        saveNotes()
-    })
+    editor_mode.querySelector('.save_btn').addEventListener('click',saveNotes)
+    modal_wrapper.addEventListener('click',saveNotes)
 
    async function saveNotes(){
        const saveEditedNote = await import('./saveEditedNote.js')
@@ -50,6 +51,6 @@ export default function editModer(div,c1,c2,e,save_btn){
         setTimeout(()=>{
             editor_mode.style.display = 'none'
             div.style.opacity = 1
-        },300)
+        },401)
     }
 }
